e01ed5fd8e04ef3bc78861d73f675922f8609e1b
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-11-19 16:31:57 -0600

    deploy commit

--- app.js
+++ app.js
@@ -18,5 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/auth', authRouter);
 app.use('/todo', todoRouter);
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 module.exports = app;
 
---
+++ build/static/js/3.efa2d698.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-todo"]=this["webpackJsonpreact-todo"]||[]).push([[3],{126:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var s=n(0),u=n(8),c=n(1);function o(){var t=Object(s.useContext)(u.a),e=t.state,n=t.dispatch;return Object(c.jsxs)("form",{onSubmit:function(t){t.preventDefault(),n({type:"LOGOUT"})},children:["Logged in as: ",Object(c.jsx)("b",{children:e.user.username}),Object(c.jsx)("input",{type:"submit",value:"Logout"})]})}}}]);
+//# sourceMappingURL=3.efa2d698.chunk.js.map
